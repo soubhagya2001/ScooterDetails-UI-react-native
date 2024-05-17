11a227aa63ccdff8fa13224b76908153ff894a63
soubhagya2001 <soubhagyaprusty36@gmail.com>
2024-05-17 22:33:38 +0530

    final edit

--- components/screens/Detail.js
+++ components/screens/Detail.js
@@ -1,21 +1,135 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View , Image} from 'react-native'
 import React from 'react'
 import { FontAwesome } from '@expo/vector-icons'
 import { Feather } from '@expo/vector-icons'
 import { TouchableOpacity } from 'react-native'
 
-const Detail = () => {
+const Detail = (props) => {
   return (
     <View style={styles.container}>
-          <View style={styles.header}>
-              <TouchableOpacity>
-                  <Feather name='chevron-left' color="#FFF" size={25} />
-              </TouchableOpacity>
+      <View style={styles.header}>
+        <TouchableOpacity onPress={() =>{props.navigation.goBack()}}>
+          <Feather name="chevron-left" color="#FFF" size={25} />
+        </TouchableOpacity>
+        <Feather name="shopping-cart" color="#FFF" size={25} />
+      </View>
+      <Image source={require("../img/2.png")} style={styles.img} />
+      <View style={styles.cont3}>
+        <Text style={styles.title}>Max Scooter</Text>
+        <Text style={styles.subtitle}>Model S1</Text>
+        <View style={styles.cont2}>
+          <Text style={{ ...styles.title, flex: 2, marginTop: 0 }}>Colors</Text>
+          <View style={styles.selected}>
+            <View style={styles.c1}></View>
+          </View>
+          <View style={styles.c2} />
+          <View style={styles.c3} />
+        </View>
+
+        <Text style={styles.text}>lorem ipsum</Text>
+        <View style={styles.cont1}>
+          <FontAwesome name="heart-o" color="#000" size={25} />
+          <TouchableOpacity style={styles.btn} onPress={() => props.navigation.navigate("Home")}>
+            <Text style={styles.btnText}>Next</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
-  )
+  );
 }
 
 export default Detail
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: 'center',
+    backgroundColor: "#121212"
+  },
+  title: {
+    fontSize: 25,
+    marginTop: 30,
+  },
+  subtitle: {
+    fontSize: 20,
+    color: "#474747",
+    marginTop: 10,
+    
+  },
+  text: {
+    fontSize: 18,
+    paddingRight: 80,
+    lineHeight: 25,
+  },
+  btn: {
+    backgroundColor: "#E24438",
+    paddingHorizontal: 60,
+    paddingVertical: 12,
+    borderRadius: 30
+  },
+  btnText: {
+    fontSize: 20,
+    color: '#FFF'
+  },
+  cont1: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    justifyContent: "space-between",
+    marginTop: 40
+  },
+  c3: {
+    height: 20,
+    width: 20,
+    borderRadius: 15,
+    backgroundColor: "#529C47",
+  },
+  c2: {
+    height: 20,
+    width: 20,
+    borderRadius: 15,
+    backgroundColor: "#529C47",
+    marginHorizontal:10
+  },
+  c1: {
+    height: 20,
+    width: 20,
+    borderRadius: 15,
+    backgroundColor: "#E2443B",
+  },
+  selected: {
+    borderColor: "#E24438",
+    height: 30,
+    width: 30,
+    borderRadius: 24,
+    borderWidth: 2,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  cont2: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: "100%",
+    marginVertical: 25
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 20,
+    paddingTop:50,
+  },
+  img: {
+    height: "45%",
+    width:"50%"
+  },
+  cont3: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    width: "100%",
+    borderRadius: 50,
+    paddingHorizontal:20,
+  },
+});
--- components/screens/Home.js
+++ components/screens/Home.js
@@ -1,61 +1,63 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native";
 const Home = (props) => {
   return (
-    <View>
-          <Image source={require("../img/1.png")} style={styles.img} />
-          <Text style={styles.title}>Max Scooter</Text>
-          <Text style={styles.detail}>
-              With an updated motor , and integrated anti - theft tech the max scooters are custom-tuned for the ultimate riding experience.
-
-          </Text>
-          <TouchableOpacity style={styles.btn}
-            onPress={()=> props.navigation.navigate("Detail")}
-          >
-              <Text style={styles.text}>Next</Text>
-          </TouchableOpacity>
+    <View style={styles.container}>
+      <Image source={require("../img/1.png")} style={styles.img} />
+      <Text style={styles.title}>Max Scooter</Text>
+      <Text style={styles.detail}>
+        With an updated motor , and integrated anti - theft tech the max
+        scooters are custom-tuned for the ultimate riding experience.
+      </Text>
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => props.navigation.navigate("Detail")}
+      >
+        <Text style={styles.text}>Next</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        backgroundColor:"#121212"
-    },
-    img: {
-        height: "50%",
-        width: "120%",
-        resizeMode:"contain"
-    },
-    title: {
-        color: "#FFF",
-        fontSize: 30,
-        marginTop:20
-
-    },
-    detail: {
-        color: "FFF",
-        fontSize: 18,
-        textAlign: "center",
-        paddingHorizontal: 20,
-        lineHeight: 30,
-        marginTop:20
-    },
-    btn: {
-        marginTop: 80,
-        backgroundColor: "E24438",
-        paddingHorizontal: 140,
-        paddingVertical: 10,
-        borderRadius:30
-    },
-    text: {
-        fontSize: 30,
-        color:"#FFF",
-    }
-})
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#121212",
+  },
+  img: {
+    height: "50%",
+    width: "120%",
+    resizeMode: "contain",
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 30,
+    marginTop: 20,
+    marginHorizontal: "26%",
+  },
+  detail: {
+    color: "#FFF",
+    fontSize: 18,
+    textAlign: "center",
+    paddingHorizontal: 20,
+    lineHeight: 30,
+    marginTop: 10,
+  },
+  btn: {
+    marginTop: 80,
+    marginHorizontal: "2%",
+    backgroundColor: "#E24438",
+    paddingHorizontal: 140,
+    paddingVertical: 10,
+    borderRadius: 30,
+  },
+  text: {
+    fontSize: 30,
+    color: "#FFF",
+  },
+});
